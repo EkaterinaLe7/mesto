1808f50476cf0dbe743e275901542f8e8d7c9c16
Ekaterina Lechitskaia <bobcat7@live.ru>
2023-05-13 13:38:03 +0200

    refactor: добавить класс Section

---
+++ src/components/Section.js
@@ -0,0 +1,21 @@
+export default class Section {
+  constructor({ data, renderer }, selector) {
+    this._initialArray = data;
+    this._renderer = renderer;
+    this._container = document.querySelector(selector);
+  }
+
+  renderItems() {
+    this._initialArray.forEach((item) => {
+      this._renderer(item)
+    });
+  }
+
+  addItem(element, addToStart) {
+    if(addToStart) {
+      this._container.prepend(element);
+    } else {
+      this._container.append(element);
+    }
+  }
+}
